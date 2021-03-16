964ed8ffaca16a28589a91ea2bc41f832537dec3
judong <judong93@naver.com>
2021-03-16 22:11:45 +0900

    프로젝트 생성 및 메인페이지 일부구현 #S04P22A205-8

---
+++ frontend/readme/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="home">
+    <div class="main-title">
+      README
+    </div>
+    <Main />
+  </div>
+</template>
+
+<script>
+import Main from '@/views/Main'
+
+export default {
+  name: 'Home',
+  components: {
+    Main,
+  },
+  watch: function () {
+
+  }
+}
+</script>
+
+<style>
+#home {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  /* z-index: 0; */
+}
+.main-title {
+  position: absolute;
+  left: 10%;
+  top: 10%;
+  height: 5%;
+  width: 10%;
+  font-size: 2.5rem;
+  z-index: 1;
+}
+</style>
