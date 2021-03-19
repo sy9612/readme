d963ca4114d7968ff9d92c7905efcae484d56a04
judong <judong93@naver.com>
2021-03-19 22:24:44 +0900

    #PJD01 - 와이어프레임 vol1 #S04P22A205-3

--- frontend/readme/src/views/Home.vue
+++ frontend/readme/src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="home">
-    <div class="main-title">
-      README
+    <div class="menu">
+      <div class="page-name">MENU</div>
+      <div class="menu-list">
+        <div>독서 현황</div>
+        <div>독서 현황</div>
+        <div>독서 현황</div>
+        <div>독서 현황</div>
+      </div> 
+    </div>
+    <div class="main">
+      <div class="main-title">
+        <span>README</span>
+        <button @click="clickMenu">MENU</button> 
+      </div>
     </div>
     <Main />
   </div>
@@ -14,6 +26,11 @@ export default {
   name: 'Home',
   components: {
     Main,
+  },
+  methods: {
+    clickMenu: function () {
+      
+    }
   },
   watch: function () {
 
@@ -28,13 +45,48 @@ export default {
   width: 100%;
   /* z-index: 0; */
 }
+.menu {
+  position: absolute;
+  box-sizing: border-box;
+  padding: 0 10%;
+  padding-top: 5%;
+  height: 100%;
+  width: 100%;
+  z-index: 2;
+  background: #d7b9a1;
+}
+.main {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+}
 .main-title {
   position: absolute;
-  left: 10%;
+  display: flex;
+  justify-content: space-between;
+  overflow: hidden;
+  /* left: 10%; */
+  padding: 0 10%;
   top: 10%;
-  height: 5%;
-  width: 10%;
+  height: 6%;
+  width: 80%;
   font-size: 2.5rem;
-  z-index: 1;
+  z-index: 3;
+}
+.page-name {
+  position: relative;
+  width: 70%;
+  left: 30%;
+}
+.menu-list {
+  position: relative;
+  left: 20%;
+  top: 10%;
+  width: 80%;
+  font-size: 2.5rem;
+  font-weight: bold;
+}
+.menu-list > div {
+  margin-bottom: 2%;
 }
 </style>
