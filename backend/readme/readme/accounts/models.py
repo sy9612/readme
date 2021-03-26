f9ad91d1d94a41e99bee0834d4e191c516891529
PSY <lizzey2@naver.com>
2021-03-26 11:54:05 +0900

    테스트용-books

---
+++ backend/readme/readme/accounts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import UserManager, AbstractUser
+
+
+class User(AbstractUser):
+    objects = UserManager(
+    )  # blank=True: 폼(입력양식)에서 빈채로 저장되는 것을 허용, DB에는 ''로 저장 # CharField 및 TextField는 blank=True만 허용, null=True 허용 X
+    nickname = models.CharField(blank=True, max_length=50)
+    gender = models.CharField(max_length=5)
+    mbti_id = models.IntegerField()
+    birth = models.DateTimeField(auto_now_add=True)
+
+
+class Dibs(models.Model):
+    dibs_id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField(blank=True, null=True)
+    book_id = models.IntegerField(blank=True, null=True)
+    dibs_date = models.DateTimeField(blank=True, null=True)
+    is_selected = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'accounts_dibs'
