b8f574bfd4fd8e066ca9f24111e4e2fb4e5a1e04
PSY <lizzey2@naver.com>
2021-03-26 16:55:41 +0900

    박서영06-MyPage.vue 테스트코드 작성

--- frontend/readme/src/router/index.js
+++ frontend/readme/src/router/index.js
@@ -1,11 +1,29 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Signup from '../components/Signup.vue'
+import Login from '../components/Login.vue'
+import MyPage from '../components/MyPage.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/MyPage',
+    name: 'MyPage',
+    component: MyPage
   },
 ]
 
