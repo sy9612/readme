9bba0c78df0713f7f72b69d15693aa9a48470905
신윤식 <yoonsik131@daum.net>
2021-03-29 22:45:34 +0900

    신윤식01 - aws배포

--- backend/readme/books/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('list', views.booklist),
-    path('search', views.search, name="search"),
-    path('<int:book_id>/', views.detail, name='detail'),
-]
--- backend/readme/readme/mbtis/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MbtisConfig(AppConfig):
-    name = 'mbtis'
