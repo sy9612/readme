1ae22e184ac474d3eacb9b8e90a312b14c8d829e
Yunsung <msmf3@naver.com>
2021-03-30 15:50:38 +0900

    김윤성06 - 독후감 기능 코드 수정2 #S04P23A205-19

--- backend/readme/reports/views.py
+++ backend/readme/reports/views.py
@@ -11,29 +11,27 @@ import sys
 sys.path.append( os.path.dirname(os.path.abspath(os.path.dirname(__file__))) )
 from books.models import Report
 
-@api_view(('GET', ))
-def report_list(request):
-    # .get()은 single object = not iterable
-    # .filter() 는 QuerySet = iterable
-    user_id = request.data['user_id']
-    report_list = Report.objects.filter(user_id = user_id)
-    # many = True : queryset이 여러 개의 아이템을 포함하고 있다.(리스트) 를 장고(DRF)에 알려줌
-    serializer = ReportSerializer(report_list, many = True)
+@api_view(('GET', 'POST'))
+def report(request, user_id):
+    if request.method == 'GET':
+        # .get()은 single object = not iterable
+        # .filter() 는 QuerySet = iterable
+        report_list = Report.objects.filter(user_id = user_id)
+        # many = True : queryset이 여러 개의 아이템을 포함하고 있다.(리스트) 를 장고(DRF)에 알려줌
+        serializer = ReportSerializer(report_list, many = True)
+
+    elif request.method == 'POST':
+        serializer = ReportSerializer(data = request.data)
+        # DRF(Django Rest Framework) 에서는 request에서 데이터를 받을 때
+        # 반드시 is_valid() 여부를 체크해야 한다.
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
     return Response(serializer.data, status = status.HTTP_200_OK)
 
-@api_view(('POST', ))
-def create_report(request):
-    serializer = ReportSerializer(data = request.data)
-    # DRF(Django Rest Framework) 에서는 request에서 데이터를 받을 때
-    # 반드시 is_valid() 여부를 체크해야 한다.
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status = status.HTTP_201_CREATED)
-    return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-
 @api_view(('GET', 'PUT', 'DELETE'))
-def report_detail(request, book_id):
-    user_id = request.data['user_id']
+def report_detail(request, user_id, book_id):
     try:
         report = Report.objects.get(Q(user_id = user_id) & Q(book_id = book_id))
     except Report.DoesNotExist:
