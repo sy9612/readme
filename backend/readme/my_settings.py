d19b134e957a841641d574e7f6a4a4ebc399f16a
Yunsung <msmf3@naver.com>
2021-04-08 17:20:51 +0900

    캡쳐 및 my_settings.py 추가

---
+++ backend/readme/my_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',    
+        'NAME': 'readme',              
+        'USER': 'root',                          
+        'PASSWORD': 'ssafy',                  
+        'HOST': 'j4a205.p.ssafy.io',                     
+        'PORT': '3306',                          
+    }
+}
+SECRET_KEY = '078cwnzwr&0mkmje9co6o2djm_0_pwfj9)+)9d70#_t!@ck7n_'
