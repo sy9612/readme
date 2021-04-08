9f7c085ccc1fd300ad15d983488c6db3bdf3df3d
Yunsung <msmf3@naver.com>
2021-04-08 12:34:03 +0900

    김윤성36 - mbti api 수정 #S04P23A205-50

--- backend/readme/mbtis/urls.py
+++ backend/readme/mbtis/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     # ex: /mbtis/3
-    path('<int:mbti_id>', views.mbti_book_list, name='mbti_book_list'),
+    path('<int:user_id>', views.mbti_book_list, name='mbti_book_list'),
 ]
